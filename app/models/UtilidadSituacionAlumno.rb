a477105a486f4a6ee676a20a9050869b0bdee2b8
cristian <cristian.sepulveda.co@usach.cl>
2020-01-04 16:13:13 -0300

    Fin versiones malla

--- app/models/UtilidadSituacionAlumno.rb
+++ app/models/UtilidadSituacionAlumno.rb
@@ -19,6 +19,11 @@ class UtilidadSituacionAlumno
         #end
     end
 
+    def obtenerVersionCarrera(rut)
+        response = HTTParty.get('http://localhost:3000/alu_Car?alu_rut='+rut)
+        @a_hash = JSON.parse(response.body) #hasheamos la respuesta para poder acceder
+    end
+
     def obtenerTipoCarrera(cod)
         response = HTTParty.get('http://localhost:3000/tcarrera?tcr_ctip='+cod)
         @a_hash = JSON.parse(response.body) #hasheamos la respuesta para poder acceder
@@ -35,13 +40,19 @@ class UtilidadSituacionAlumno
         @a_hash = JSON.parse(response.body) #hasheamos la respuesta para poder acceder
     end
 
+    def obtenerversionMalla(cod)
+        #response = HTTParty.get('http://localhost:3000/malla?ma_ver_plan=4&ma_ccar='+cod)
+        response = HTTParty.get('http://localhost:3000/malla?_sort=ma_ver_plan&_order=desc&ma_ccar='+cod)
+        @a_hash = JSON.parse(response.body) #hasheamos la respuesta para poder acceder
+    end
+
     def obtenerCalificacionesConRut(rut)
         response = HTTParty.get('http://localhost:3000/calificacion?cal_sit_alu=A&cal_rut='+rut)
         @a_hash = JSON.parse(response.body) #hasheamos la respuesta para poder acceder
     end
 
     def obtenerTodasAsignaturas()
-        response = HTTParty.get('http://localhost:3000/asignaturaa')
+        response = HTTParty.get('http://localhost:3000/asignatura')  #asignaturaa funka
         @a_hash = JSON.parse(response.body) #hasheamos la respuesta para poder acceder
     end
 
@@ -49,4 +60,11 @@ class UtilidadSituacionAlumno
         response = HTTParty.get('http://localhost:3000/asignatura?asi_cod='+cod)
         @a_hash = JSON.parse(response.body) #hasheamos la respuesta para poder acceder
     end
+
+    def obtenerAsignaturaDeMalla(cod)
+        response = HTTParty.get('http://localhost:3000/malla?ma_asign='+cod)
+        @a_hash = JSON.parse(response.body) #hasheamos la respuesta para poder acceder
+    end
 end
+
+
