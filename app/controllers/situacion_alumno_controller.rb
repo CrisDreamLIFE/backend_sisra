1a75121081583f22aefb0842b8135c5905cd9112
Kakoo8 <44330230+Kakoo8@users.noreply.github.com>
2020-01-16 17:51:17 -0300

    Update situacion_alumno_controller.rb

--- app/controllers/situacion_alumno_controller.rb
+++ app/controllers/situacion_alumno_controller.rb
@@ -8,7 +8,7 @@
       obj = UtilidadSituacionAlumno.new()
       rutAlu = params[:rut]
       alumno = obj.obtenerAlumnoConRut(rutAlu)
-      jsona = {"caca": 515, "pene": "sfsdf"}
+      jsona = {"asdasd": 515, "asdasd": "sfsdf"}
       #@jsona = otro
       render :json => jsona
     end
